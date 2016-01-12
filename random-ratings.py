79fc48b4e9be3e2906ba9c1cb637534b37f2d3e4
U-Lenovo-PC\rmurphy <bekkam@users.noreply.github.com>
2016-01-11 21:33:30 -0800

    uncomment code to process user input; exercise completed.

--- random-ratings.py
+++ random-ratings.py
@@ -17,6 +17,22 @@ def update_random_restaurant(filename):
         # dictionary.keys() use random() to select index of the restaurant 
         
 
+# # your code goes here
+# def process_user_input():
+#     """Asks the user for restaurant and rating until user quits"""
 
+#     user_data = {}
+
+#     while True:
+#         user_prompt = raw_input("Would you like to add a restaurant rating? y/n ")
+#         if user_prompt.lower() == "n":
+#             return user_data
+#             break
+#         elif user_prompt.lower() == "y":
+#             user_input_restaurant = raw_input("Please enter a restaurant: ")
+#             user_input_score = raw_input("Please enter a restarant score: ")
+#             user_data[user_input_restaurant] = user_input_score
+#         else:
+#             print "This is not a valid answer"
 
 update_random_restaurant("scores.txt")
--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,43 +1,39 @@
-# # your code goes here
-# def process_user_input():
-#     """Asks the user for restaurant and rating until user quits"""
-    
-#     user_data = {}
+def process_user_input():
+    """Asks the user for restaurant and rating until user quits"""
 
-#     while True:
-#         user_prompt = raw_input("Would you like to add a restaurant rating? y/n ")
-#         if user_prompt.lower() == "n":
-#             return user_data
-#             break
-#         elif user_prompt.lower() == "y":
-#             user_input_restaurant = raw_input("Please enter a restaurant: ")
-#             user_input_score = raw_input("Please enter a restarant score: ")
-#             user_data[user_input_restaurant] = user_input_score
-#         else:
-#             print "This is not a valid answer"
+    user_data = {}
+
+    while True:
+        user_prompt = raw_input("Would you like to add a restaurant rating? y/n ")
+        if user_prompt.lower() == "n":
+            return user_data
+            break
+        elif user_prompt.lower() == "y":
+            user_input_restaurant = raw_input("Please enter a restaurant: ")
+            user_input_score = raw_input("Please enter a restarant score: ")
+            user_data[user_input_restaurant] = user_input_score
+        else:
+            print "This is not a valid answer."
 
 
 def alphabetize_restaurant_ratings(filename):
     """Returns each restaurant and its rating, in alphabetical order
 
-        after processing user input.
+    after processing user_input
     """
-
-    raw_data = process_user_input()
 
     restaurant_log = open(filename)
 
+    restaurants = {}
+
     for line in restaurant_log:
         data = line.rstrip().split(":")
-        print type(data)
-#         raw_data[data[0]] = data[1]
+        restaurants[data[0]] = data[1]
 
-#     result = sorted(raw_data.items(), key = lambda row: row[0])
+    alphabetized_restaurants = sorted(restaurants.items(), key=lambda row: row[0])
 
-#     for restaurant in result:
-#         print "%s is rated at %s." % (restaurant[0], restaurant[1])
-
-
-# alphabetize_restaurant_ratings("scores.txt")
+    for restaurant in alphabetized_restaurants:
+        print "%s is rated at %s." % (restaurant[0], restaurant[1])
 
 
+alphabetize_restaurant_ratings("scores.txt")
