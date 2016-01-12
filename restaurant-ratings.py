ab80c0ecc9e13429b080d3f7a788de8623a6ae14
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 17:18:51 -0800

    update function name and method chaining

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,16 +1,22 @@
 # your code goes here
 
 
-def alphabetized_restaurant_ratings(filename):
+def alphabetize_restaurant_ratings(filename):
     """Returns each restaurant and its rating, in alphabetical order"""
+
+    # while True:
+    #     user_prompt = raw_input("Would you like to add a restaurant rating?")
+    #        if 
+
+    # user_input_restaurant = raw_input("Please enter a restaurant")
+    # user_input_score = raw_input("Please enter a restarant score")
 
     restaurant_log = open(filename)
 
     raw_data = {}
 
     for line in restaurant_log:
-        line = line.rstrip()
-        data = line.split(":")
+        data = line.rstrip().split(":")
         raw_data[data[0]] = data[1]
 
     result = sorted(raw_data.items(), key = lambda row: row[0])
@@ -20,3 +26,5 @@ def alphabetized_restaurant_ratings(filename):
 
 
 alphabetized_restaurant_ratings("scores.txt")
+
+
