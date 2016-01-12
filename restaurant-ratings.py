2870864d7658e9f13be6cc9d4740552a0cc23076
Hackbright Student <info@hackbrightacademy.com>
2016-01-11 18:03:35 -0800

    created random-restaurant.py see line 16 to proceede. incomplete

--- restaurant-ratings.py
+++ restaurant-ratings.py
@@ -1,20 +1,20 @@
-# your code goes here
-def process_user_input():
-    """Asks the user for restaurant and rating until user quits"""
+# # your code goes here
+# def process_user_input():
+#     """Asks the user for restaurant and rating until user quits"""
     
-    user_data = {}
+#     user_data = {}
 
-    while True:
-        user_prompt = raw_input("Would you like to add a restaurant rating? y/n ")
-        if user_prompt.lower() == "n":
-            return user_data
-            break
-        elif user_prompt.lower() == "y":
-            user_input_restaurant = raw_input("Please enter a restaurant: ")
-            user_input_score = raw_input("Please enter a restarant score: ")
-            user_data[user_input_restaurant] = user_input_score
-        else:
-            print "This is not a valid answer"
+#     while True:
+#         user_prompt = raw_input("Would you like to add a restaurant rating? y/n ")
+#         if user_prompt.lower() == "n":
+#             return user_data
+#             break
+#         elif user_prompt.lower() == "y":
+#             user_input_restaurant = raw_input("Please enter a restaurant: ")
+#             user_input_score = raw_input("Please enter a restarant score: ")
+#             user_data[user_input_restaurant] = user_input_score
+#         else:
+#             print "This is not a valid answer"
 
 
 def alphabetize_restaurant_ratings(filename):
@@ -29,14 +29,15 @@ def alphabetize_restaurant_ratings(filename):
 
     for line in restaurant_log:
         data = line.rstrip().split(":")
-        raw_data[data[0]] = data[1]
+        print type(data)
+#         raw_data[data[0]] = data[1]
 
-    result = sorted(raw_data.items(), key = lambda row: row[0])
+#     result = sorted(raw_data.items(), key = lambda row: row[0])
 
-    for restaurant in result:
-        print "%s is rated at %s." % (restaurant[0], restaurant[1])
+#     for restaurant in result:
+#         print "%s is rated at %s." % (restaurant[0], restaurant[1])
 
 
-alphabetize_restaurant_ratings("scores.txt")
+# alphabetize_restaurant_ratings("scores.txt")
 
 
